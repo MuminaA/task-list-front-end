d4ec0b39702e8210599b47a96981d366bff6a19d
Gina Song <gina0song@gmail.com>
2025-12-18 10:19:52 -0800

    Update App.jsx with useEffect to fetch tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,70 @@
-import TaskList from './components/TaskList.jsx';
 import './App.css';
-import { useState } from 'react';
+import TaskList from './components/TaskList.jsx';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const TASKS = [
-  {
-    id: 1,
-    title: 'Mow the lawn',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'Cook Pasta',
-    isComplete: true,
-  },
-];
+
+
+
+// const TASKS = [
+//   {
+//     id: 1,
+//     title: 'Mow the lawn',
+//     isComplete: false,
+//   },
+//   {
+//     id: 2,
+//     title: 'Cook Pasta',
+//     isComplete: true,
+//   },
+// ];
+
+
+
+// Base URL for the Task List API
+const kbaseURL = 'http://localhost:5000';
+
+// Function to get all tasks from the API
+const getAllTasksAPI = () => {
+  return axios.get(`${kbaseURL}/tasks`)
+    .then(response => response.data)
+    .catch(error => console.log(error));
+};
+
+// Function to convert ONE API task format to frontend task format
+const convertFromAPI = (apiTask) => {
+  const newTask = {
+    id: apiTask.id,
+    title: apiTask.title,
+    isComplete: apiTask.is_complete,
+  };
+  return newTask;
+};
+
+
 
 const App = () => {
-  // store tasks in state
-  const[tasks, setTasks] = useState(TASKS);
+  // state to hold tasks array
+  const[tasks, setTasks] = useState([]);
+
+  // loads tasks and updates state
+  const getAllTasks = () => {
+    return getAllTasksAPI()
+      .then(tasks => {
+        // Map over array and convert each task individually
+        const newTasks = tasks.map(convertFromAPI);
+        setTasks(newTasks);  // Update state
+      });
+  };
+
+  // useEffect to load tasks on component mount
+  useEffect(() => {
+    getAllTasks();
+  }, []);
+
+
+
+
 
   // function to toggle task completion
   const toggleComplete = (id) => {
